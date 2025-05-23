ff6ce961609de1384bcc0c9f210c4f2a042a9789
Gabriel_Viera <gabrielviera5nac@gmail.com>
2025-05-23 00:11:33 -0500

    cambio 3

--- Codigo_Control_Motor_dc/Codigo_Control_Motor_dc.ino
+++ Codigo_Control_Motor_dc/Codigo_Control_Motor_dc.ino
@@ -1,5 +1,5 @@
 const int motorPin = 9;
-const int velocidad_baja = 90; // Valor PWM de 0 a 255
+const int velocidad_baja = 80; // Valor PWM de 0 a 255
 
 void setup() {
   pinMode(motorPin, OUTPUT);
